93e75f29ec7444c5878e7f147136defb8af6a633
Abhay D <a.deshpande012@gmail.com>
2018-04-17 19:03:22 -0700

    modified shell script

--- run_bot.sh
+++ run_bot.sh
@@ -1,7 +1,11 @@
 #!/bin/sh
 while true
 do
-	nohup python3 ~/discord_bot/bot.py
-	echo 'Running again!'
-	sleep 1
+	python3 ~/discord_bot/bot.py>&1
+	if [ $? = 0 ]; then
+		break
+	else
+		echo "Running again!">&2
+		sleep 1
+	fi
 done
