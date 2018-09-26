bfa7866445e914af2a2b399973c161b035e31c0f
Abhay D <a.deshpande012@gmail.com>
2018-09-26 15:23:40 -0700

    Reload meme information every time, so memes can be hotswapped

--- memer.py
+++ memer.py
@@ -9,25 +9,48 @@ last_id = None # Prevent replying to duplicate messages
 config_cooldown_trigger = '!cooldown'
 last_mention = {}
 mention_cooldown = 5 # Won't meme again if mentioned multiple times within timespan
-    
-with open('resources/memes.info') as file:
-    users = file.read().strip()
-    users = json.loads(users)
 
-with open('resources/gif_memes.info') as file:
-    gif_info = json.loads(file.read().strip())
+def load_meme_info():
+    """Load the file from disk containing the user id and memes
+    
+    Returns
+    -------
+    dict
+        Dict describing the users and their memes. Keys are strings with the discord id of the user. (username#tag)
+        Values are a list of strings, which are the memes to be sent out.
+    """
+    with open('resources/memes.info') as file:
+        users = file.read().strip()
+        users = json.loads(users)
+    return users
+
+def load_gif_info():
+    """Load the file from disk containing the gif information.
+    
+    Returns
+    -------
+    dict
+        Keys are strings with the discord id of the user. (username#tag)
+        Values are a string, signifying the path to the gif to be sent.
+    """
+
+    with open('resources/gif_memes.info') as file:
+        gif_info = json.loads(file.read().strip())
+    return gif_info
 
 def register(_bot):
     global bot
     bot = _bot
-    global users
+
+    meme_info = load_meme_info()
+    gif_info = load_gif_info()
 
     for trigger in gif_info:
         bot.register_trigger(trigger, create_gif_sender(gif_info[trigger]))
 
     bot.register_trigger(config_cooldown_trigger, config_cooldown)
-    bot.debug(users)
-    for user_id in users:
+    bot.debug(meme_info)
+    for user_id in meme_info:
         username, discriminator = user_id.split('#')
         bot.register_mention_subscriber(username, discriminator, get_message)
 
@@ -43,16 +66,16 @@ async def get_message(client, message):
     if last_id != message.id:
         last_id = message.id
         mentions = message.mentions
+        meme_info = load_meme_info()
         for user in mentions:
             user_id = '{}#{}'.format(user.name, user.discriminator)
-            global users
-            if user_id in users:
+            if user_id in meme_info.keys():
                 curr_time = time.time()
                 if user_id in last_mention:
                     if curr_time - last_mention[user_id] <= mention_cooldown:
                         continue
                 last_mention[user_id] = curr_time
-                await client.send_message(message.channel, random.choice(users[user_id]))
+                await client.send_message(message.channel, random.choice(meme_info[user_id]))
 
 async def config_cooldown(client, message):
     user = '{}#{}'.format(message.author.name, message.author.discriminator)
